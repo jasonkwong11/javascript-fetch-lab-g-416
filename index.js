e6b30499dd24d6b4f1c978c62e850547ab624a03
Jason <jasonkwong11@gmail.com>
2017-01-01 13:06:58 -0500

    initial work on fetch method

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ function createIssue() {
 }
 
 function showResults(json) {
+
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(repo)
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function getToken() {
